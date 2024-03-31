08d004faffc7f45b85b7dffc17c5a35713d737e5
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-03-31 00:47:06 -0600

    Actualización de la función de búsqueda***

--- src/components/CalculatorForm.jsx
+++ src/components/CalculatorForm.jsx
@@ -3,130 +3,111 @@ import style from "../styles/calculatorStyle.module.css";
 
 function CalculatorForm() {
     const [input, setInput] = useState("");
-    const [previousNumber, setPreviousNumber] = useState("");
-    const [currentNumber, setCurrentNumber] = useState("");
-    const [operator, setOperator] = useState("");
-    const [error, setError] = useState("");
+    const [resultDisplayed, setResultDisplayed] = useState(false);
 
-    const addToInput = val => {
-        if (!error) {
-            setInput(prevInput => prevInput === "0" && val !== "." ? val : prevInput + val);
+    const handleNumberClick = (e) => {
+        let currentString = input;
+        let lastChar = currentString[currentString.length - 1];
+
+        if (!resultDisplayed) {
+            setInput(currentString + e.target.textContent);
+        } else if (resultDisplayed && (lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/")) {
+            setResultDisplayed(false);
+            setInput(currentString + e.target.textContent);
+        } else {
+            setResultDisplayed(false);
+            setInput(e.target.textContent);
         }
     };
 
-    const add = () => {
-        if (!error) {
-            setOperation("+");
+    const handleOperatorClick = (e) => {
+        let currentString = input;
+        let lastChar = currentString[currentString.length - 1];
+
+        if (lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/") {
+            let newString = currentString.substring(0, currentString.length - 1) + e.target.textContent;
+            setInput(newString);
+        } else if (currentString.length === 0) {
+            console.log("Enter a number first");
+        } else {
+            setInput(currentString + e.target.textContent);
         }
     };
 
-    const subtract = () => {
-        if (!error) {
-            setOperation("-");
+    const handleEqualClick = () => {
+        let inputString = input;
+        let numbers = inputString.split(/\+|\-|\*|\//g);
+        let operators = inputString.replace(/[0-9]|\./g, "").split("");
+
+        let divideIndex = operators.indexOf("/");
+        while (divideIndex !== -1) {
+            numbers.splice(divideIndex, 2, numbers[divideIndex] / numbers[divideIndex + 1]);
+            operators.splice(divideIndex, 1);
+            divideIndex = operators.indexOf("/");
         }
+
+        let multiplyIndex = operators.indexOf("*");
+        while (multiplyIndex !== -1) {
+            numbers.splice(multiplyIndex, 2, numbers[multiplyIndex] * numbers[multiplyIndex + 1]);
+            operators.splice(multiplyIndex, 1);
+            multiplyIndex = operators.indexOf("*");
+        }
+
+        let subtractIndex = operators.indexOf("-");
+        while (subtractIndex !== -1) {
+            numbers.splice(subtractIndex, 2, numbers[subtractIndex] - numbers[subtractIndex + 1]);
+            operators.splice(subtractIndex, 1);
+            subtractIndex = operators.indexOf("-");
+        }
+
+        let addIndex = operators.indexOf("+");
+        while (addIndex !== -1) {
+            numbers.splice(addIndex, 2, parseFloat(numbers[addIndex]) + parseFloat(numbers[addIndex + 1]));
+            operators.splice(addIndex, 1);
+            addIndex = operators.indexOf("+");
+        }
+
+        setInput(numbers[0].toString());
+        setResultDisplayed(true);
     };
 
-    const multiply = () => {
-        if (!error) {
-            setOperation("*");
-        }
-    };
-
-    const divide = () => {
-        if (!error) {
-            if (input !== "0") {
-                setOperation("/");
-            } else {
-                setError("Error: División por cero");
-            }
-        }
-    };
-
-    const clearInput = () => {
+    const handleClearClick = () => {
         setInput("");
-        setError("");
-    };
-
-    const setOperation = op => {
-        if (input) {
-            if (previousNumber && operator) {
-                if ((operator === '+' || operator === '-') && (op === '*' || op === '/')) {
-                    // Si el operador actual tiene mayor precedencia, apilar la operación actual
-                    stack.push({ number: parseFloat(input), operator: op });
-                } else {
-                    // Si no, evaluar la operación anterior y apilar el resultado
-                    evaluate();
-                    stack.push({ number: parseFloat(input), operator: op });
-                }
-            } else {
-                setPreviousNumber(input);
-                setOperator(op);
-            }
-            setInput("");
-        }
-    };
-
-    const evaluate = () => {
-        try {
-            let result = parseFloat(previousNumber);
-            while (stack.length > 0) {
-                const { number, operator } = stack.shift();
-                switch (operator) {
-                    case '+':
-                        result += number;
-                        break;
-                    case '-':
-                        result -= number;
-                        break;
-                    case '*':
-                        result *= number;
-                        break;
-                    case '/':
-                        result /= number;
-                        break;
-                    default:
-                        break;
-                }
-            }
-            setInput(result.toString());
-        } catch (err) {
-            setError("Error: Operación no válida");
-        }
     };
 
     return (
         <div className={style.calculator}>
-            <div className={style.input} id="input">{error || input}</div>
+            <div className={style.input} id="input">{input}</div>
             <div className={style.buttons}>
                 <div className={style.operators}>
-                    <div onClick={add}>+</div>
-                    <div onClick={subtract}>-</div>
-                    <div onClick={multiply}>&times;</div>
-                    <div onClick={divide}>&divide;</div>
+                    <div onClick={handleOperatorClick}>+</div>
+                    <div onClick={handleOperatorClick}>-</div>
+                    <div onClick={handleOperatorClick}>*</div>
+                    <div onClick={handleOperatorClick}>/</div>
                 </div>
                 <div className={style.leftPanel}>
                     <div className={style.numbers}>
-                        <div onClick={() => addToInput("7")}>7</div>
-                        <div onClick={() => addToInput("8")}>8</div>
-                        <div onClick={() => addToInput("9")}>9</div>
+                        <div onClick={handleNumberClick}>7</div>
+                        <div onClick={handleNumberClick}>8</div>
+                        <div onClick={handleNumberClick}>9</div>
                     </div>
                     <div className={style.numbers}>
-                        <div onClick={() => addToInput("4")}>4</div>
-                        <div onClick={() => addToInput("5")}>5</div>
-                        <div onClick={() => addToInput("6")}>6</div>
+                        <div onClick={handleNumberClick}>4</div>
+                        <div onClick={handleNumberClick}>5</div>
+                        <div onClick={handleNumberClick}>6</div>
                     </div>
                     <div className={style.numbers}>
-                        <div onClick={() => addToInput("1")}>1</div>
-                        <div onClick={() => addToInput("2")}>2</div>
-                        <div onClick={() => addToInput("3")}>3</div>
+                        <div onClick={handleNumberClick}>1</div>
+                        <div onClick={handleNumberClick}>2</div>
+                        <div onClick={handleNumberClick}>3</div>
                     </div>
                     <div className={style.numbers}>
-                        <div onClick={() => addToInput("0")}>0</div>
-                        <div onClick={() => addDecimal(".")}>.</div>
-                        <div id="clear" onClick={clearInput}>C</div>
+                        <div onClick={handleNumberClick}>0</div>
+                        <div onClick={handleNumberClick}>.</div>
+                        <div id="clear" onClick={handleClearClick}>C</div>
                     </div>
                 </div>
-                <div className={style.equal} onClick={evaluate}>=</div>
+                <div className={style.equal} id="result" onClick={handleEqualClick}>=</div>
             </div>
         </div>
     );
