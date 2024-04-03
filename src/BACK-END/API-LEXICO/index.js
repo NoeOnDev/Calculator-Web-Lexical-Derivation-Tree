4f8627e9acc44a10d9e0d563123eed42ed983ec0
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-04-03 15:21:24 -0600

    Agregado API-LEXICO al backend

---
+++ src/BACK-END/API-LEXICO/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(bodyParser.json());
+
